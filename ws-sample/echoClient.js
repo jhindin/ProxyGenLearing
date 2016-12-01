0fbafda707f21db72fe55aa5015b6ca1752790b0
Joseph Hindin <hindin@nkloom.com>
2016-12-01 20:14:38 +0200

    echo client

---
+++ ws-sample/echoClient.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+var WebSocket = require('ws');
+
+var url = "http://localhost:8895/echo";
+if (process.argv.length > 2)
+    url = process.argv[2];
+
+var ws = new WebSocket(url);
+
+ws.on('open', function open() {
+    ws.send('something');
+});
+
+ws.on('message', function(data, flags) {
+    console.log("message arrived ", flags + ", data = ", data);
+  // flags.binary will be set if a binary data is received.
+  // flags.masked will be set if the data was masked.
+});
+
+console.log("Exiting");
