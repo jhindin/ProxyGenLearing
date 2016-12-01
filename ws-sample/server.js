01db24165a5321dbdad0b9f0dad6abf61f98a5d7
Joseph Hindin <hindin@nkloom.com>
2016-12-01 20:08:13 +0200

    ws

--- ws-sample/server.js
+++ ws-sample/server.js
@@ -5,6 +5,14 @@ var express = require("express");
 app = express();
 app.use(express.static('public'));
 
+var expressWs = require('express-ws')(app);
+
+app.ws('/echo', function(ws, req) {
+    ws.on('message', function(msg) {
+        ws.send(msg);
+    });
+});
+
 var port = "8895";
 
 if (process.argv.length > 2)
