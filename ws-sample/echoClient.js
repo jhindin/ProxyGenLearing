4d9d1113703aeb03158a4092790b71ecdbfcac32
Joseph Hindin <hindin@gmail.com>
2016-12-16 12:45:24 +0200

    argument parsing, debug printout removed

--- ws-sample/echoClient.js
+++ ws-sample/echoClient.js
@@ -3,9 +3,39 @@
 var WebSocket = require('ws');
 var cliParser = require('minimist');
 
+function optUnknown(a)
+{
+    c = a.charAt(0);
+    if (c == '-') {
+	console.log("Unknown options", a)
+	return false;
+    }
+    return true;
+}
+
+var help="<url>\n" +
+    "\topen websocket on given URL, by default http://localhost:80/echo";
+
 var url = "http://localhost:8895/echo";
-if (process.argv.length > 2)
-    url = process.argv[2];
+
+parsedArgs = cliParser(process.argv.slice(2),
+                       { boolean: ["h"], stopEarly: true,
+                         unknown: optUnknown });
+
+
+if (parsedArgs.h || parsedArgs.help) {
+    console.log(help);
+    process.exit(0);
+}
+
+if (parsedArgs._.length > 1) {
+    console.log("Extra args");
+    console.log(help);
+    process.exit(0);
+}
+
+if (process.argv.length == 1)
+    url = parsedArgs._[0];
 
 var ws = new WebSocket(url);
 
@@ -17,7 +47,7 @@ ws.on('message', function(data, flags) {
     console.log("message arrived ", flags + ", data = ", data);
   // flags.binary will be set if a binary data is received.
   // flags.masked will be set if the data was masked.
-  ws.close();
+    
+    ws.close();
 });
 
-console.log("Exiting");
