1620bceae9d882a9f13dc2a903b5e20264ae7971
Joseph Hindin <hindin@nkloom.com>
2017-01-02 15:03:53 +0200

    close on message

--- ws-sample/binEchoClient.js
+++ ws-sample/binEchoClient.js
@@ -6,6 +6,8 @@ var url = "http://localhost:8895/echo";
 if (process.argv.length > 2)
     url = process.argv[2];
 
+console.log("url = ", url)
+
 var ws = new WebSocket(url);
 
 ws.on('open', function open() {
@@ -16,6 +18,7 @@ ws.on('message', function(data, flags) {
     console.log("message arrived ", flags + ", data = ", data);
   // flags.binary will be set if a binary data is received.
   // flags.masked will be set if the data was masked.
+  ws.close();
 });
 
 console.log("Exiting");
